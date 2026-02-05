bdf89d58c9020f0d2e2ecc5fb06fbf88d0e4d865
juicybob123 <okruh2028@kehillahstudent.org>
2026-02-05 09:25:40 -0800

    adwadawdwad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def alt_caps(original_string):
+   
     """Convert a string to Alternating Caps
 
     Args:
@@ -9,10 +10,33 @@ def alt_caps(original_string):
         >>> print(alt_caps("Alternating Capitalization"))
         aLtErNaTiNg CaPiTaLiZaTiOn
     """
-    new_string = ""
+    string= ""
+    x = 1
+    y = 0
+    new_string = original_string
+    for charachter in new_string:
+        if y%2 == 0:
+            string = charachter.lower()
+            print(string)
+        if y%2 == 1:
+            string = charachter.upper()
+            print(string)
+        y= y+1
+    
+    #     (new_string[x:x+1]).upper()
+    #     x= x+2
+    #     string = string + new_string[x:x+1]
+    #     (new_string[y:y+1]).upper()
+    #     y = y+2
+    #     string = string + new_string
+    # print(string)
+            
+    
 
     # YOUR CODE HERE
+   
 
     return new_string
+
 
 print(alt_caps("Alternating Capitalization"))
